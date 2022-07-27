c91d080b34ad8bf37d3d0a980b1a6b91095774b5
Adam Topczewski <gnomowisko@gmail.com>
2022-07-27 14:57:38 +0200

    basic api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors())
 // Dane statyczne
 let notes = [
     {
@@ -23,6 +25,19 @@ let notes = [
     }
 ]
 
+const requestLogger = (req, res, next) => {
+    console.log('Method: ', req.method)
+    console.log('Path: ', req.path)
+    console.log('Body: ', req.body)
+    console.log('---')
+    next()
+}
+
+app.use(express.json())
+app.use(requestLogger)
+
+
+
 app.get('/', (req, res) => {
     res.send('<h1>Elo z homepage</h1>')
 })
@@ -31,7 +46,56 @@ app.get('/api/notes', (req, res) => {
     res.json(notes)
 })
 
-const PORT = 3000;
+app.get('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const note = notes.find(note => note.id === id)
+
+    if(note) {
+        res.json(note)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    res.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0 
+    ? Math.max(...notes.map(n => n.id))
+    : 0;
+    return maxId + 1;
+}
+
+app.post('/api/notes', (req, res) => {
+    const body = req.body;
+    if(!body.content) {
+        return res.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId()
+    }
+    notes.concat(note)
+    res.json(note)
+})
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({error: 'unkown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
