4ba1cc6225d0a7aec98142ff0bb2acf80806a5a2
Adam Topczewski <gnomowisko@gmail.com>
2022-07-28 11:00:05 +0200

    finished tutorial part 3c

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const Note = require('./models/note');
+const { response } = require('express');
+const { update } = require('./models/note');
+
 
 const requestLogger = (req, res, next) => {
     console.log('Method: ', req.method)
@@ -28,17 +31,24 @@ app.get('/api/notes', (req, res) => {
     })
 })
 
-app.get('/api/notes/:id', (req, res) => {
-    Note.findById(req.params.id).then(note => {
-        res.json(note)
+app.get('/api/notes/:id', (req, res, next) => {
+    Note.findById(req.params.id)
+    .then(note => {
+        if(note) {
+            res.json(note)
+        } else {
+            res.status(404).end()
+        }
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    res.status(204).end()
+app.delete('/api/notes/:id', (req, res, next) => {
+    Note.findByIdAndDelete(req.params.id)
+        .then( result => {
+            res.status(204).end()            
+        })
+        .catch( error => next(error))
 })
 
 app.post('/api/notes', (req, res) => {
@@ -59,11 +69,38 @@ app.post('/api/notes', (req, res) => {
     })
 })
 
+app.put('/api/notes', (req, res, next) => {
+    const body = req.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
 const unknownEndpoint = (req, res) => {
     res.status(404).send({error: 'unkown endpoint'})
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malfromed id'})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
